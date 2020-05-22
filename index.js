ab2693e473d4bab4475e9bd3470091b8b1e39a71
Arvin  Alhissou <arvin.alhissou@gmail.com>
2020-05-22 18:50:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById('main').remove();
+let newText = document.createElement('h1');
+newText.setAttribute('id', 'victory');
+newText.innerContent = "Flatiron school is the champion";
+newText.className = "victory";
+let newHeader=docunmet.querySelector('h1#victory');
+body.appendChild(newText);
