4ab4e4f9238710488a1609f564e5553ca7e79873
Arvin  Alhissou <arvin.alhissou@gmail.com>
2020-05-22 18:55:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Write your code here!
-document.getElementById('main').remove();
-let newHeader = document.createElement('h1');
-newText.setAttribute('id', 'victory');
-newText.innerContent = "Flatiron school is the champion";
+let element = document.querySelector("body");
+element.removeChild(document.querySelector("main#main"))
+
+let newHeader=document.querySelector("h1#victory")
+
+newHeader.innerHTML='is the champion'
